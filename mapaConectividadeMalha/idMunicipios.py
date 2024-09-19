9346c5c2c1566d7b6575560d0a3ebc2bd6a81108
João Roberto Galvão de Oliveira <joaorobertogalvao.2008@gmail.com>
2024-09-19 20:44:51 -0300

    problema na coluna CD_MUN

--- mapaConectividadeMalha/idMunicipios.py
+++ mapaConectividadeMalha/idMunicipios.py
@@ -1,36 +1,21 @@
-from typing import List, Any
 import requests
-import pandas as pd
+from typing import Dict, Optional
 
-
-df_municipios = pd.read_csv('parahybaCities.csv', engine='python')
-
-df_municipios['CD_MUN'] = df_municipios['CD_MUN'].astype(str)
-
-
-malhas: List[Any] = []
-
-for cd_num in df_municipios['CD_MUN']:
-
-    url = f"https://servicodados.ibge.gov.br/api/v3/malhas/{cd_num}"
-
+def obter_malha(codigo_municipio: str) -> Optional[Dict]:
+    url = f"https://servicodados.ibge.gov.br/api/v3/malhas/municipios/{codigo_municipio}?formato=application/vnd.geo+json"
     try:
-
         response = requests.get(url)
         response.raise_for_status()
 
+        if response.headers.get('Content-Type') == 'application/vnd.geo+json':
+            return response.json()
+        else:
+            print(f"Formato de resposta inesperado para o código {codigo_municipio}.")
+            return None
 
-        malhas.append(response.json())
-
-    except requests.exceptions.HTTPError:
-        print(f"Erro para o código de município {cd_num}.")
-        malhas.append(None)
-
+    except requests.exceptions.HTTPError as e:
+        print(f"Erro HTTP para o código {codigo_municipio}: {e}")
+        return None
     except Exception as e:
-        print(f"Ocorreu um erro para o código {cd_num}: {e}. Adicionando valor vazio.")
-        malhas.append(None)
-
-df_malhas = pd.DataFrame(malhas)
-
-
-print(df_malhas.head())
+        print(f"Ocorreu um erro para o código {codigo_municipio}: {e}")
+        return None
